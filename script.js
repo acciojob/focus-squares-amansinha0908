c3707a3f16aae30e76645f26acc20a7317dab613
amansinha0908 <139490539+amansinha0908@users.noreply.github.com>
2025-09-22 11:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const squares = document.querySelectorAll(".square");
 // Loop through each square
 squares.forEach(sq => {
   // Mouse enters: change other squares to Coffee
-  sq.addEventListener("mouseenter", () => {
+  sq.addEventListener("mouseover", () => {
     squares.forEach(otherSq => {
       if (otherSq !== sq) {
         otherSq.style.backgroundColor = "#6F4E37"; // Coffee
@@ -14,7 +14,7 @@ squares.forEach(sq => {
   });
 
   // Mouse leaves: reset all squares to Lavender
-  sq.addEventListener("mouseleave", () => {
+  sq.addEventListener("mouseout", () => {
     squares.forEach(s => {
       s.style.backgroundColor = "#E6E6FA"; // Lavender
     });
